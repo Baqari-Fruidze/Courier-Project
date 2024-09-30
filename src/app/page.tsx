27c31bd5aec3be83ac3d5279fc7213e5b68cef58
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-09-30 22:42:08 +0400

    json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>olla</h1>;
+export default async function Home() {
+  const res = await fetch("http://localhost:3001/users");
+  const data = await res.json();
+  console.log(data[0].city);
+  return (
+    <>
+      {data.map((item) => {
+        return (
+          <div key={item.id} className="border border-red-900">
+            <h1 className="text-3xl text-red-400">{item.address.city}</h1>
+            <h2>{item.address.zipcode}</h2>
+          </div>
+        );
+      })}
+    </>
+  );
 }
---
+++ src/app/register/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" />
+      </div>
+      <div>
+        <label htmlFor=""></label>
+        <input type="text" />
+      </div>
+      <input type="radio" />
+    </div>
+  );
+}
