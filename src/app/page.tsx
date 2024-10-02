122945858e09ff69b2a5fc21ab2a78bd66defdc6
Baqari-Fruidze <baqarifruidze@gmail.com>
2024-10-02 20:31:05 +0400

    middleware fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { getServerSession } from "next-auth";
-import { options } from "./api/auth/[...nextauth]/option";
-
 export default async function Home() {
-  const session = await getServerSession(options);
-  console.log(session);
   return (
     <>
-      <h1>loged in</h1>
-      <p>{session?.user?.name}</p>
+      <h1>main</h1>
     </>
   );
 }
